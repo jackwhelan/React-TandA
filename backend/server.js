bda2f5db5894e8fb77031b042fd1a56e4fc571f5
jackwhelan <jackw97@live.ie>
2020-02-23 20:27:21 +0000

    Created basic server with db access

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+const db = require('./config/keys').mongoURI;
+
+mongoose
+    .connect(db, {
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+    })
+    .then(() => console.log('DB Connected!'))
+    .catch(err => {
+        console.log(`DB Connection Error: ${ err.message }`);
+    });
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
