e5588299fb00ffd0437c73df7d43cd1359ccc967
jackwhelan <jackw97@live.ie>
2020-03-22 19:33:49 +0000

    Testing clocking routes

--- backend/routes/clocking.js
+++ backend/routes/clocking.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const checkAuth = require('./middleware/check-auth');
 
 // Clocking model
 const { Clocking, ClockingValidation } = require('../models/Clocking');
@@ -7,10 +8,14 @@ const { Clocking, ClockingValidation } = require('../models/Clocking');
 // @route   GET /clocking
 // @desc    Get All Clockings
 // @access  Public
-router.get('/', (req, res) => {
+router.get('/', checkAuth, (req, res) => {
     Clocking.find()
         .sort({ date: -1 })
         .then(clocking => res.json(clocking));
+});
+
+router.post('/in', (req, res) => {
+    
 });
 
 module.exports = router;
