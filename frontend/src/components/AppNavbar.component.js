4c8abf34333bb0b1715184a89b09a5a0b5f47861
jackwhelan <jackw97@live.ie>
2020-03-22 19:31:45 +0000

    Added localstorage sessions, user dependant nav
    Also added fontawesome icons and logging in and out.

--- frontend/src/components/AppNavbar.component.js
+++ frontend/src/components/AppNavbar.component.js
@@ -30,7 +30,70 @@ class AppNavbar extends Component {
         return path;
     }
 
+    leftNav = () => {
+        if (localStorage.getItem('USER_ID')) {
+            return (
+                <Nav className="mr-auto" navbar>
+                    <NavItem>
+                        <NavLink href="/"><i class="fas fa-home"></i> Home</NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink href="/Dashboard"><i class="fas fa-columns"></i> Dashboard</NavLink>
+                    </NavItem>
+                </Nav>
+            )
+        }
+        else {
+            return (
+                <Nav className="mr-auto" navbar>
+                    <NavItem>
+                        <NavLink href="/"><i class="fas fa-home"></i> Home</NavLink>
+                    </NavItem>
+                </Nav>
+            )
+        }
+    }
+
+    rightNav = () => {
+        if (localStorage.getItem('USER_ID')) {
+            return (
+                <Nav className="ml-auto" navbar>
+                    <NavItem>
+                        <NavLink href="/Account"><i class="fas fa-user-circle"></i> My Account</NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink href="#logout" onClick={this.handleLogout}><i class="fas fa-sign-out-alt"></i> Log Out</NavLink>
+                    </NavItem>
+                </Nav>
+            )
+        }
+        else {
+            return (
+                <Nav className="ml-auto" navbar>
+                    <NavItem>
+                        <NavLink href="/Login"><i class="fas fa-sign-in-alt"></i> Log In</NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink href="/Register"><i class="fas fa-user-plus"></i> Register</NavLink>
+                    </NavItem>
+                </Nav>
+            )
+        }
+    }
+
+    handleLogout = () => {
+        localStorage.clear()
+        this.setState({
+            redirectToReferrer: true
+        });
+    }
+
     render() {
+        const redirectToReferrer = this.state.redirectToReferrer;
+        if (redirectToReferrer === true) {
+            return <Redirect to="/Goodbye" />
+        }
+
         return (
             <div>
                 <Navbar color="dark" dark expand="sm">
@@ -38,22 +101,12 @@ class AppNavbar extends Component {
                         <NavbarBrand href="/">Staff Portal <span className="text-muted">| { this.getPath() }</span></NavbarBrand>
                         <NavbarToggler onClick={this.toggle} />
                         <Collapse isOpen={this.state.isOpen} navbar>
-                            <Nav className="mr-auto" navbar>
-                                <NavItem>
-                                    <NavLink href="/">Home</NavLink>
-                                </NavItem>
-                                <NavItem>
-                                    <NavLink href="/Dashboard">Dashboard</NavLink>
-                                </NavItem>
-                            </Nav>
-                            <Nav className="ml-auto" navbar>
-                                <NavItem>
-                                    <NavLink href="/Login">Log In</NavLink>
-                                </NavItem>
-                                <NavItem>
-                                    <NavLink href="/Register">Register</NavLink>
-                                </NavItem>
-                            </Nav>
+                            {
+                                this.leftNav()
+                            }
+                            {
+                                this.rightNav()
+                            }
                         </Collapse>
                     </Container>
                 </Navbar>
