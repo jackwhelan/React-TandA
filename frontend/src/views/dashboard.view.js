dea960d77d8d71952e436931a5c1f7a865ee631f
jackwhelan <jackw97@live.ie>
2020-03-22 19:33:42 +0000

    Added dashboard greeting

--- frontend/src/views/dashboard.view.js
+++ frontend/src/views/dashboard.view.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
 import Navbar from '../components/AppNavbar.component';
-import QR from '../components/QR.component';
 
 class DashboardView extends Component {
     render() {
         return (
             <div>
                 <Navbar />
-                <div className="container bg-light">
-                    <QR />
+                <div className="container bg-light p-5">
+                    <h4>Hello {localStorage.getItem('USER_FIRSTNAME')}</h4>
                 </div>
             </div>
         )
