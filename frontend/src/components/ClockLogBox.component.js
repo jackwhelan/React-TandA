15785ee8f7c4e72dc7e0c1186c11155a086c17f5
jackwhelan <jackw97@live.ie>
2020-04-01 13:37:14 +0100

    Added clocklogbox component

---
+++ frontend/src/components/ClockLogBox.component.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class ClockLogBox extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+        }
+    }
+
+    render() {
+        return (
+            <div className="card">
+                <div className="card-body">
+                    <h1 className="card-title text-center mb-2">Data here</h1>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default ClockLogBox;
