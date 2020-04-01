e01f1d5cd1e69e82e6aa7e84ee4a424192ae4768
jackwhelan <jackw97@live.ie>
2020-04-01 13:35:46 +0100

    Adding clocking API routes
    /clocking/status/:id
    /clocking/list/:id
    /clocking/in
    /clocking/out

--- backend/routes/clocking.js
+++ backend/routes/clocking.js
@@ -1,20 +1,78 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
 
 // Clocking model
-const { Clocking, ClockingValidation } = require('../models/Clocking');
+const Clocking = require('../models/Clocking');
+const {User} = require('../models/User');
 
 // @route   GET /clocking
 // @desc    Get All Clockings
-// @access  Public
 router.get('/', (req, res) => {
     Clocking.find()
         .sort({ date: -1 })
         .then(clocking => res.json(clocking));
 });
 
+// @route   GET /clocking/status/:id
+// @desc    Get whether a user is clocked in or out by ID
+router.get('/status/:id', (req, res) => {
+    User.findById(req.params.id)
+    .then(user => {
+        lastClock = user.clocking.sort((a, b) => {
+            return b.datetime - a.datetime;
+        })[0];
+        res.json(lastClock.status)
+    })
+    .catch(err => {
+        console.log(err);
+    })
+})
+
+// @route   GET /clocking/list/:id
+// @desc    Gets a list of clocking data by user ID
+router.get('/list/:id', (req, res) => {
+    User.findById(req.params.id)
+        .then(user => {
+            lastClock = user.clocking.sort((a, b) => {
+                return b.datetime - a.datetime;
+            });
+            res.json(lastClock)
+        })
+})
+
+// @route   GET /clocking/in
+// @desc    Clock in
 router.post('/in', (req, res) => {
-    
+    newClocking = new Clocking({
+        status: "in"
+    });
+
+    User.findOneAndUpdate(
+        { username: req.body.username },
+        { $push: { clocking: newClocking } },
+        { useFindAndModify: false }
+    )
+    .then(user => {
+        res.json(user);
+    });
+});
+
+// @route   GET /clocking/out
+// @desc    Clock out
+router.post('/out', (req, res) => {
+    newClocking = new Clocking({
+        status: "out"
+    });
+
+    User.findOneAndUpdate(
+        { username: req.body.username },
+        { $push: { clocking: newClocking } },
+        { useFindAndModify: false }
+    )
+        .then(user => {
+            res.json(user);
+        });
 });
 
 module.exports = router;
