2efc836cffd1fa9c199b527b627d36df3dcc563e
jackwhelan <jackw97@live.ie>
2020-04-08 22:55:39 +0100

    Added clockings refresh on clock

--- frontend/src/views/clocking.view.js
+++ frontend/src/views/clocking.view.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Navbar from '../components/AppNavbar.component';
-import ClockStatusBox from '../components/ClockStatusBox.component';
-import ClockLogBox from '../components/ClockLogBox.component';
+import ClockStatusBox from '../components/clocking/ClockStatusBox.component';
+import ClockLogBox from '../components/clocking/ClockLogBox.component';
 
 class ClockingView extends Component {
     render() {
@@ -12,7 +12,7 @@ class ClockingView extends Component {
                     <h4>Hello {localStorage.getItem('USER_FIRSTNAME')}</h4><hr className="bg-black mb-5" />
                     <ClockStatusBox/>
                     <hr className="bg-black mt-5 mb-5"></hr>
-                    <ClockLogBox/>
+                    <ClockLogBox key={Math.random()}/>
                 </div>
             </div>
         )
